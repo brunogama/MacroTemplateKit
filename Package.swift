177455965b574671590aa6b6c79be454c6b4e79b
Bruno <bruno@naboo.local>
2026-03-01 04:34:14 -0300

    chore: update swift-syntax dependency to 602.0.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0")
+    .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0")
   ],
   targets: [
     .target(
